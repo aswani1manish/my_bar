3642e87ff21a53233a48c625d86ee411f18d00ed
aswani1manish <48398248+aswani1manish@users.noreply.github.com>
2025-12-30 21:01:18 -0500

    Change apiUrl to relative path for production

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -5,5 +5,5 @@
 // For local development: http://localhost:5000/api
 // For PythonAnywhere: https://yourusername.pythonanywhere.com/api
 var APP_CONFIG = {
-    apiUrl: 'http://localhost:5000/api'
+    apiUrl: '/api'
 };
